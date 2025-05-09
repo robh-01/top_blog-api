a834626c9a9e635ffc1e5cbe48789e5eaa6ffa3e
Roshan Bhusal <128559779+robh-01@users.noreply.github.com>
2025-05-09 10:21:40 +0545

    Add comment query functions

---
+++ server/db/commentQueries.js
@@ -0,0 +1,49 @@
+import prisma from "./prismaClient.js";
+
+//takes id of a post to add a comment on and comment object with the data of the comment
+async function addComment(postId, comment) {
+  const addedComment = await prisma.comment.create({
+    data: {
+      ...comment,
+      postId,
+    },
+  });
+
+  return addedComment;
+}
+
+async function getCommentById(commentId) {
+  const comment = await prisma.comment.findUnique({
+    where: {
+      id: commentId,
+    },
+  });
+
+  return comment;
+}
+
+// takes id of the post to edit(commentId) with the contents to update(in comment object)
+// and updates the comment
+async function editComment(commentId, comment) {
+  const updatedComment = await prisma.comment.update({
+    where: {
+      id: commentId,
+    },
+    data: {
+      ...comment,
+    },
+  });
+  return updatedComment;
+}
+
+async function deleteComment(commentId) {
+  const deletedComment = prisma.comment.delete({
+    where: {
+      id: commentId,
+    },
+  });
+
+  return deletedComment;
+}
+
+export { addComment, getCommentById, editComment, deleteComment };
