13496afbf459a2fb9ee000541894a7169e992e7a
Roshan Bhusal <128559779+robh-01@users.noreply.github.com>
2025-05-09 08:53:30 +0545

    Add queries to get all the posts and edit a post

--- server/db/postQueries.js
+++ server/db/postQueries.js
@@ -15,4 +15,31 @@ async function getPosts() {
   return posts;
 }
 
-export { createPost, getPosts };
+async function getPostById(postId) {
+  const post = await prisma.post.findUnique({
+    where: {
+      id: postId,
+    },
+    include: {
+      comments: true,
+    },
+  });
+
+  return post;
+}
+
+// takes id of the post to update(postId) with the contents to update(in post object)
+// and updates the post
+async function editPost(postId, post) {
+  const updatedPost = await prisma.post.update({
+    where: {
+      id: postId,
+    },
+    data: {
+      ...post,
+    },
+  });
+  return updatedPost;
+}
+
+export { createPost, getPosts, getPostById, editPost };
