ba6dad84357aaa74bdfec834d81cfe4ab971e1c5
Roshan Bhusal <128559779+robh-01@users.noreply.github.com>
2025-05-10 23:39:01 +0545

    Authenticate user before fetching userFromId

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -3,21 +3,17 @@ const userRouter = Router();
 
 import * as userController from "../controllers/userController.js";
 
-// jwt authentication middleware usage demo
-// import { authenticateJWT } from "../middleware/authMiddleware.js";
+// jwt authentication middleware
+import { authenticateJWT } from "../middleware/authMiddleware.js";
 
 // userRouter.get("", userController.getAllUsersHandler);
 userRouter.post("/signup", userController.createUserHandler);
 userRouter.post("/login", userController.loginUserHandler);
 
-// jwt authentication middleware usage demo
-// userRouter.get("/protected", authenticateJWT, (req, res, next) => {
-//   res.status(400).json({
-//     status: "success",
-//     message: "You are in a protected route",
-//   });
-// });
-
-userRouter.get("/:userId", userController.getUserFromIdHandler);
+userRouter.get(
+  "/:userId",
+  authenticateJWT,
+  userController.getUserFromIdHandler
+);
 
 export { userRouter };
