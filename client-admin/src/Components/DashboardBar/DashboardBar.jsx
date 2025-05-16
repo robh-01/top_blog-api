540aa36528dbadeb4c6acf175610581edadbbb8a
Roshan Bhusal <128559779+robh-01@users.noreply.github.com>
2025-05-16 21:48:19 +0545

    Add the dashboard bar component

--- client-admin/src/Components/DashboardBar/DashboardBar.jsx
+++ client-admin/src/Components/DashboardBar/DashboardBar.jsx
@@ -1,3 +1,15 @@
+import styles from "./DashboardBar.module.css";
+
 export function DashboardBar() {
-  return <>this is dashboard bar</>;
+  return (
+    <>
+      <div className={styles["dashboard-bar"]}>
+        <p className={styles["dashboard-bar--logo"]}>Tellin</p>
+        <div className={styles["dashboard-bar--right-container"]}>
+          <p className={styles["dashboard-bar--title"]}>Blog List</p>
+          <button className={styles["dashboard-bar--btn"]}>Create new blog</button>
+        </div>
+      </div>
+    </>
+  );
 }
