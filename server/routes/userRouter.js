7ae61a22fab54ed4e6ceb92f577960949fd7bcb8
Roshan Bhusal <128559779+robh-01@users.noreply.github.com>
2025-05-16 09:25:56 +0545

    Add routes to validate user and author token

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -4,11 +4,25 @@ const userRouter = Router();
 import * as userController from "../controllers/userController.js";
 
 // jwt authentication middleware
-import { authenticateJWT } from "../middleware/authMiddleware.js";
+import { authenticateJWT, authorizeAdmin } from "../middleware/authMiddleware.js";
 
 // userRouter.get("", userController.getAllUsersHandler);
 userRouter.post("/signup", userController.createUserHandler);
 userRouter.post("/login", userController.loginUserHandler);
+
+userRouter.get("/validate/token", authenticateJWT, (req, res) => {
+  res.status(200).json({
+    status: "success",
+    message: "Token is valid.",
+  });
+});
+
+userRouter.get("/validate/author", authenticateJWT, authorizeAdmin, (req, res) => {
+  res.status(200).json({
+    status: "success",
+    message: "Token is valid and is of author.",
+  });
+});
 
 userRouter.get(
   "/:userId",
