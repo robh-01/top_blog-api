d9b95dd75cb42ad5a0025459c12a92323b8f9ad5
Roshan Bhusal <128559779+robh-01@users.noreply.github.com>
2025-05-17 00:20:59 +0545

    Render blogs to the page after fetching

--- client-admin/src/Components/BlogLister/BlogLister.jsx
+++ client-admin/src/Components/BlogLister/BlogLister.jsx
@@ -1,36 +1,30 @@
+import { useContext } from "react";
+import { DashboardContext } from "../Dashboard/Dashboard";
+import styles from "./BlogLister.module.css";
 import { BlogTile } from "../BlogTile/BlogTile";
 
-import styles from "./BlogLister.module.css";
-
-const blog = {
-  id: 6,
-  title: "A very Title of the post A very Title of the post ",
-  content:
-    "content of the page content of the page content of the page content of the page content of the page content of the page content of the page content of the page ",
-  createdAt: "2025-05-10T18:40:50.708Z",
-  published: false,
-  publishedAt: null,
-  userId: 14,
-};
+// const blog = {
+//   id: 6,
+//   title: "A very Title of the post A very Title of the post ",
+//   content:
+//     "content of the page content of the page content of the page content of the page content of the page content of the page content of the page content of the page ",
+//   createdAt: "2025-05-10T18:40:50.708Z",
+//   published: false,
+//   publishedAt: null,
+//   userId: 14,
+// };
 
 export function BlogLister() {
+  const { loading, error, blogs } = useContext(DashboardContext);
+
   return (
     <>
       <div className={styles["blog-lister"]}>
-        <BlogTile blog={blog} />
-        <BlogTile blog={blog} />
-        <BlogTile blog={blog} />
-        <BlogTile blog={blog} />
-        <BlogTile blog={blog} />
-        <BlogTile blog={blog} />
-        <BlogTile blog={blog} />
-        <BlogTile blog={blog} />
-        <BlogTile blog={blog} />
-        <BlogTile blog={blog} />
-        <BlogTile blog={blog} />
-        <BlogTile blog={blog} />
-        <BlogTile blog={blog} />
-        <BlogTile blog={blog} />
+        {loading
+          ? "Loading....."
+          : error
+          ? `${error}`
+          : blogs.map((blog) => <BlogTile blog={blog} key={blog.id} />)}
       </div>
     </>
   );
--- client-admin/src/Components/Dashboard/Dashboard.jsx
+++ client-admin/src/Components/Dashboard/Dashboard.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState, createContext } from "react";
 import { Outlet, useNavigate } from "react-router-dom";
 
 import { DashboardBar } from "../DashboardBar/DashboardBar";
@@ -6,8 +6,13 @@ import { DashboardNav } from "../DashboardNav/DashboardNav";
 
 import styles from "./Dashboard.module.css";
 
+export const DashboardContext = createContext();
+
 export default function Dashboard() {
   const navigate = useNavigate();
+  const [blogs, setBlogs] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const token = localStorage.getItem("token");
@@ -15,12 +20,14 @@ export default function Dashboard() {
       navigate("/");
     } else {
       //for canceling the fetch request in the cleanup function
-      const controller = new AbortController();
-      const signal = controller.signal;
+      const controller1 = new AbortController();
+      const controller2 = new AbortController();
+      const signal1 = controller1.signal;
+      const signal2 = controller2.signal;
 
       // Validate the token with the server
       fetch("http://localhost:3000/user/validate/author", {
-        signal,
+        signal: signal1,
         method: "GET",
         mode: "cors",
         headers: {
@@ -33,12 +40,45 @@ export default function Dashboard() {
           }
         })
         .catch((err) => {
+          if (err.name === "AbortError") return; //ignore abort errors(errors that arise from cancelling fetch)
           console.error("Token validation error:", err);
+        });
+
+      //fetch all the posts from the server(db)
+      fetch("http://localhost:3000/post", {
+        signal: signal2,
+        method: "GET",
+        mode: "cors",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+        .then((response) => {
+          return response.json();
+        })
+        .then((responseData) => {
+          if (responseData.status === "failure") {
+            // failure happens due to user not being logged in(this fetch job is not to check whether
+            //user is logged in or not. that is why throwing a generic error)
+            throw new Error("Invalid or expired token.");
+          } else {
+            setBlogs([...responseData.data.posts]);
+            setLoading(false);
+          }
+        })
+        .catch((err) => {
+          if (err.name === "AbortError") return; //ignore abort errors(errors that arise from cancelling fetch)
+          console.error("Error fetching blogs", err);
+          setError(
+            "Some error occurred while fetching the blogs. Please try again later."
+          );
+          setLoading(false);
         });
 
       return () => {
         // Abort the fetch request if the component unmounts
-        controller.abort();
+        controller1.abort();
+        controller2.abort();
       };
     }
   }, [navigate]);
@@ -49,7 +89,9 @@ export default function Dashboard() {
         <div className={styles["dashboard-lower-container"]}>
           <DashboardNav />
           <div className={styles["dashboard-container--main"]}>
-            <Outlet />
+            <DashboardContext.Provider value={{ blogs, error, loading }}>
+              <Outlet />
+            </DashboardContext.Provider>
           </div>
         </div>
       </div>
